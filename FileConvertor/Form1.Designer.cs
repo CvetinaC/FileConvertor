89fc9cc4e1ef30113266ddd311fa468ae8a3f539
Cvetina Cekina <cvetina.cekina97@gmail.com>
2018-05-04 18:13:50 +0300

    Initial

---
+++ FileConvertor/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace FileConvertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbFormat = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.lbConvertedItems = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(45, 59);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(291, 59);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(400, 59);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cbFormat
+            // 
+            this.cbFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFormat.FormattingEnabled = true;
+            this.cbFormat.Items.AddRange(new object[] {
+            ".txt",
+            ".doc",
+            ".csv",
+            ".xls"});
+            this.cbFormat.Location = new System.Drawing.Point(141, 59);
+            this.cbFormat.Name = "cbFormat";
+            this.cbFormat.Size = new System.Drawing.Size(121, 21);
+            this.cbFormat.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lbConvertedItems
+            // 
+            this.lbConvertedItems.FormattingEnabled = true;
+            this.lbConvertedItems.Location = new System.Drawing.Point(161, 111);
+            this.lbConvertedItems.Name = "lbConvertedItems";
+            this.lbConvertedItems.Size = new System.Drawing.Size(177, 134);
+            this.lbConvertedItems.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 263);
+            this.Controls.Add(this.lbConvertedItems);
+            this.Controls.Add(this.cbFormat);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "File Convertor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cbFormat;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListBox lbConvertedItems;
+    }
+}
+
---
+++ FileConvertor/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileConvertor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Open a file";
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "(*.txt ,*.doc,*.csv,*.xls)|*.txt;*.doc;*.csv;*.xls";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string pathOpen = openFileDialog1.FileName;
+                using (FileStream fs = new FileStream(pathOpen, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            lbConvertedItems.Items.Add(sr.ReadLine() + Environment.NewLine);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string items = lbConvertedItems.Text;
+            string pathOpen = openFileDialog1.FileName;
+            items = pathOpen;
+            if(cbFormat.SelectedItem != null)
+            {
+            switch (cbFormat.SelectedIndex)
+            {
+                case 0:
+                    {
+                        string extension = Path.GetExtension(pathOpen);
+                        pathOpen.Replace(extension, ".txt");
+                        items.Replace(items, ".txt");
+                        break;
+                    }
+                case 1:
+                    {
+                        string extension = Path.GetExtension(pathOpen);
+                        pathOpen.Replace(extension, ".doc");
+                        items.Replace(items, ".doc");
+                        break;
+                    }
+                case 2:
+                    {
+                        string extension = Path.GetExtension(pathOpen);
+                        pathOpen.Replace(extension, ".csv");
+                        items.Replace(items, ".csv");
+                        break;
+                    }
+                case 3:
+                    {
+                        string extension = Path.GetExtension(pathOpen);
+                        pathOpen.Replace(extension, ".xls");
+                        items.Replace(items, ".xls");
+                        break;
+                    }
+
+            }
+            }
+            else
+            {
+                MessageBox.Show("You have to choose file format first !" , "Warning" , MessageBoxButtons.OK,MessageBoxIcon.Warning );
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Save file";
+            saveFileDialog1.FileName = "";
+            switch (cbFormat.SelectedIndex)
+            {
+                case 0:
+                    {
+                        saveFileDialog1.Filter = "Text | *.txt";
+                        break;
+                    }
+                case 1:
+                    {
+                        saveFileDialog1.Filter = "Doc|*.doc";
+                        break;
+                    }
+                case 2:
+                    {
+                        saveFileDialog1.Filter = "Csv|*.csv";
+                        break;
+                    }
+                case 3:
+                    {
+                        saveFileDialog1.Filter = "Xls|*.xls";
+                        break;
+                    }
+            }
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        string path = saveFileDialog1.FileName;
+                        using (FileStream fs = new FileStream(path, FileMode.Create))
+                        {
+                            using (StreamWriter sw = new StreamWriter(fs))
+                            {
+                                for (int i = 0; i < lbConvertedItems.Items.Count; i++)
+                                {
+                                    sw.WriteLine(lbConvertedItems.Items[i]);
+                                }
+                            }
+                        }
+                    }
+
+            
+        }
+    }
+}
+    
+
